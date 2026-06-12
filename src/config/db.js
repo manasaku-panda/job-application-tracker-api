198f98a3da21d3fc9aec261deda27cb0aacf0907
manasapanda <manasa.panda@oditeksolutions.com>
2026-06-12 16:40:34 +0530

    modified:   db setup for a deployement

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,23 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging : console.log
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT, // ADD THIS
+        dialect: 'mysql',
+
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+
+        logging: false
+    }
+);
 
 module.exports = sequelize;
